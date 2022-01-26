9d0f63027a984179d11d4e8496cc6de6371c6b78
Semen <semmmen8@gmail.com>
2022-01-26 16:31:32 +0300

    Декомпозиция кода для контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -10,23 +10,23 @@ def app(request):
     return fixture
     
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.creation_contact(Contact(name="Semen1", midname="-", lastname="Vishnyakov",
-                                 nickname="Antigon", title="boss", company="Stenn",
-                                 address="Moscow", home_phone="3", mobile_phone="9852064950",
-                                 work_phone="QA", fax="-", email="semmmen8@gmail.com",
-                                 email2="-", email3="-", homepage="stenn.ru", bday="1", bmonth="January",
-                                 byear="1996", aday="1", amonth="January", ayear="2021", address2="-",
-                                 phone2="-", notes="-"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(name="Semen1", midname="-", lastname="Vishnyakov",
+                       nickname="Antigon", title="boss", company="Stenn",
+                       address="Moscow", home_phone="3", mobile_phone="9852064950",
+                       work_phone="QA", fax="-", email="semmmen8@gmail.com",
+                       email2="-", email3="-", homepage="stenn.ru", bday="1", bmonth="January",
+                       byear="1996", aday="1", amonth="January", ayear="2021", address2="-",
+                       phone2="-", notes="-"))
+    app.session.logout()
 
 def test_add_empty_contact(app):
-    app.login(username="admin", password="secret")
-    app.creation_contact(Contact(name="", midname="", lastname="",
-                                 nickname="", title="", company="",
-                                 address="", home_phone="", mobile_phone="",
-                                 work_phone="", fax="", email="",
-                                 email2="", email3="", homepage="", bday="", bmonth="",
-                                 byear="", aday="", amonth="", ayear="", address2="",
-                                 phone2="", notes=""))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(name="", midname="", lastname="",
+                       nickname="", title="", company="",
+                       address="", home_phone="", mobile_phone="",
+                       work_phone="", fax="", email="",
+                       email2="", email3="", homepage="", bday="", bmonth="",
+                       byear="", aday="", amonth="", ayear="", address2="",
+                       phone2="", notes=""))
+    app.session.logout()
